2e124829efbb99cdde01f56e87fc492c2a483748
Sandip <146024901+QuantumCoderrr@users.noreply.github.com>
2025-04-18 16:09:02 +0530

    Update urls.py

--- backend/vault/urls.py
+++ backend/vault/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from .views import VaultItemListCreate, VaultItemDetail
+
+urlpatterns = [
+    path('', VaultItemListCreate.as_view(), name='vault-list-create'),
+    path('<int:pk>/', VaultItemDetail.as_view(), name='vault-detail'),
+]
 
