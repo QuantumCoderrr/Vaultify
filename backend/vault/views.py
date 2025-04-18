f3fcb13e282a7cc551df236bcaef5e2cc3ee8b79
Sandip <146024901+QuantumCoderrr@users.noreply.github.com>
2025-04-18 16:09:19 +0530

    Update views.py

--- backend/vault/views.py
+++ backend/vault/views.py
@@ -1,1 +1,18 @@
+from rest_framework import generics, permissions
+from .models import VaultItem
+from .serializers import VaultItemSerializer
+from .permissions import IsOwner
+
+class VaultItemListCreate(generics.ListCreateAPIView):
+    queryset = VaultItem.objects.all()
+    serializer_class = VaultItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class VaultItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = VaultItem.objects.all()
+    serializer_class = VaultItemSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
 
