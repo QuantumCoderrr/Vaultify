7e2808832e2cbbecad91186e084b59e06d29442d
Sandip <146024901+QuantumCoderrr@users.noreply.github.com>
2025-04-18 16:09:55 +0530

    Update utils.py

--- backend/vault/utils.py
+++ backend/vault/utils.py
@@ -1,1 +1,12 @@
+import base64
+from cryptography.fernet import Fernet
+
+def generate_key():
+    return Fernet.generate_key()
+
+def encrypt_data(data, key):
+    return Fernet(key).encrypt(data.encode()).decode()
+
+def decrypt_data(token, key):
+    return Fernet(key).decrypt(token.encode()).decode()
 
